4ef5b98471fa096cfe895b3dfcf5de9c281534fd
A01799815 <a01799815@tec.mx>
2023-10-17 20:52:01 +0000

    Update selectionsort

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
 
+void selectionsort(std::vector<int>& data){ // Selectionsort(Data: values[])
+    for(int i=0; i<data.size(); ++i){       //     For i = 0 To <length of values> - 1
+        int value = data.at(i);             //Find the item that belongs in position i.
+        int min = i;                        //         <Find the smallest item with index j >= i.>
+        for(int j=i; j<data.size(); ++j){
+            if(data.at(j) < data.at(min)){
+                min = j;
+            }
+        }
+        data.at(i) = data.at(min);          //         <Swap values[i] and values[j].>
+        data.at(min) = value;
+    }                                       //     Next i
+}                                           // End Selectionsort
+
 std::vector<int> fill_random(int size, int max_value){
 
     std::vector<int> data(size);
@@ -18,21 +32,6 @@ void toString(std::vector<int>& data){
     }
     std::cout <<"\n";
 }
-
-
-void selectionsort(std::vector<int>& data){ // Selectionsort(Data: values[])
-    for(int i=0; i<data.size(); ++i){       //     For i = 0 To <length of values> - 1
-        int value = data.at(i);             //Find the item that belongs in position i.
-        int min = i;                        //         <Find the smallest item with index j >= i.>
-        for(int j=i; j<data.size(); ++j){
-            if(data.at(j) < data.at(min)){
-                min = j;
-            }
-        }
-        data.at(i) = data.at(min);          //         <Swap values[i] and values[j].>
-        data.at(min) = value;
-    }                                       //     Next i
-}                                           // End Selectionsort
 
 int main(){
     std::vector<int> example = fill_random(30,99);
