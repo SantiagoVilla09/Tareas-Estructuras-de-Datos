17e9c8d32145fb58b9575bd6f7bed1a25f2565f0
SantiagoVilla09 <santiagovillazon2001@gmail.com>
2023-10-17 20:46:02 +0000

    Codigo Insertion

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,11 +1,3 @@
-//  Insertionsort(Data: values[])
-//     For i = 0 To <length of values> - 1
-//         // Move item i into position in the sorted part of the array.
-//         < Find the first index j where j < i and values[j] > values[i].>
-//         <Move the item into position j.>
-//   Next i
-// End Insertionsort
-
 #include <iostream>
 #include <vector>
 
@@ -20,8 +12,26 @@ std::vector<int> fill_random(int size, int max_value){
     return data;
 }
 
+//  Insertionsort(Data: values[])
+//     For i = 0 To <length of values> - 1
+//         // Move item i into position in the sorted part of the array.
+//         < Find the first index j where j < i and values[j] > values[i].>
+//         <Move the item into position j.>
+//   Next i
+// End Insertionsort
+
+void Insertionsort(std::vector<int>& data){
+    for(i = 0; i < data.size - 1; ++i){
+        int min_index = i;
+        for(j = i + 1,){
+
+        }
+
+    }
+}
+
 int main(){
-    std::vector<int> ejemplo = fill_random(1000,99);
+    std::vector<int> ejemplo = fill_random(100,99);
     for(int i=0; i < ejemplo.size(); ++i){
         std::cout << ejemplo.at(i) << " ";
     }
